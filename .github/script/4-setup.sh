5e8288a57069552e0a9ba3dc561491c6dedaff10
Artis3n <dev@artis3nal.com>
2023-01-15 15:25:25 -0500

    feat: add step 4 and final wrap-up

--- .github/script/4-setup.sh
+++ .github/script/4-setup.sh
@@ -15,9 +15,15 @@ bound_issuer="https://token.actions.githubusercontent.com" \
 oidc_discovery_url="https://token.actions.githubusercontent.com"
 
 # Create a secret
+vault kv put secret/staging access_token=abc123
 vault kv put secret/production access_token=xyz456
-# Add a policy
-vault policy write main-policy - << EOF
+# Add OIDC policies
+vault policy write staging-policy - << EOF
+path "secret/data/staging" {
+  capabilities = ["read"]
+}
+EOF
+vault policy write prod-policy - << EOF
 path "secret/data/production" {
   capabilities = ["read"]
 }
